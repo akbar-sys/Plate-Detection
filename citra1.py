223b15ba1d37dc453c50b484f03c7b782598e2d6
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2021-06-20 17:16:23 +0800

    Update citra1.py

--- citra1.py
+++ citra1.py
@@ -1,27 +1,45 @@
-import cv2 as cv
-import imutils as im
+import numpy as np
+import cv2
+import  imutils
 
-image = cv.imread("car_image.jpg")
-# image = im.resize(image, width=500)
+# Read the image file
+image = cv2.imread('plat7.jpg')
 
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-blur = cv.bilateralFilter(gray,11,17,17)
-edgeDet = cv.Canny(blur, 170,200)
+# Resize the image - change width to 500
+image = imutils.resize(image, width=500)
 
-(cnts, _) = cv.findContours(edgeDet.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:30]
-numberPlateCnt = None
+# Display the original image
+cv2.imshow("Original Image", image)
 
+# RGB to Gray scale conversion
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("1 - Grayscale Conversion", gray)
+
+# Noise removal with iterative bilateral filter(removes noise while preserving edges)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+cv2.imshow("2 - Bilateral Filter", gray)
+
+# Find Edges of the grayscale image
+edged = cv2.Canny(gray, 170, 200)
+cv2.imshow("4 - Canny Edges", edged)
+
+# Find contours based on Edges
+(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:30] #sort contours based on their area keeping minimum required area as '30' (anything smaller than this will not be considered)
+NumberPlateCnt = None #we currently have no Number plate contour
+
+# loop over our contours to find the best possible approximate contour of number plate
 count = 0
 for c in cnts:
-    peri = cv.arcLength(c, True)
-    approx = cv.approxPolyDP(c, 0.02 * peri, True)
-    if len(approx) == 4:
-        NumberPlateCnt = approx
-        break
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:  # Select the contour with 4 corners
+            NumberPlateCnt = approx #This is our approx Number Plate Contour
+            break
 
-cv.drawContours(image, [NumberPlateCnt], -1, (0, 255, 0), 3)
-# cv.imwrite("hasildeteksi.jpg", image)
-cv.imshow("plat nomor kendaraan", image)
 
-cv.waitKey(0)
+# Drawing the selected contour on the original image
+cv2.drawContours(image, [NumberPlateCnt], -1, (0,255,0), 3)
+cv2.imshow("Final Image With Number Plate Detected", image)
+
+cv2.waitKey(0) #Wait for user input before closing the images displayed
